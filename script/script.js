a460127982e4e560fa445469bf55df03fda9bab3
yIgoreKMaN <y1876@mail.ru>
2018-10-30 15:45:58 +0300

    111

--- script/script.js
+++ script/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-	var height_block =$(".container-header-next").height();
+	var height_block = $(".container-header-next").height();
 	$("section .o-section-header, section .o-section-content").height(height_block);
+alert($(".item-skills").offset().top);
+	if ($(document).scrollTop() == $(" .item-skills ").offset().top) {
+		alert($(".item-skills").offset().top);
+		$(".progressing-bar-in").css({"width": "70%"});
+	
+	}
+	
 })

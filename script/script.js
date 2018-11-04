e1f65a758e596e13ad468a92fed629443370074b
y1876 <y1876@mail.ru>
2018-11-04 22:01:54 +0300

    +test

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 	var height_block = $(".container-header-next").height();
 	$("section .o-section-header, section .o-section-content").height(height_block);
-alert($(".item-skills").offset().top);
-	if ($(document).scrollTop() == $(" .item-skills ").offset().top) {
-		alert($(".item-skills").offset().top);
-		$(".progressing-bar-in").css({"width": "70%"});
-	
+
+	if ($(document).hasClass("item-skills")) {
+		alert("Class i-s yes");
+		// statement
 	}
+	$(document).scroll(function(){
+		if ($(document).scrollTop() == $(".item-skills").offset().top - $(".item-skills").width()) {
+			alert("Это работает");
+			$(".progressing-bar-in").css({"width": "70%"});		
+		}
+	})
+
+	
+
+
+
 	
 })

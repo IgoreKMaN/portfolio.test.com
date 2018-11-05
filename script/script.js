665c90e0583a793fd1418a4e8ea192560e343997
yIgoreKMaN <y1876@mail.ru>
2018-11-05 08:46:03 +0300

    111

--- script/script.js
+++ script/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 	var height_block = $(".container-header-next").height();
 	$("section .o-section-header, section .o-section-content").height(height_block);
-alert($(".item-skills").offset().top);
-	if ($(document).scrollTop() == $(" .item-skills ").offset().top) {
-		alert($(".item-skills").offset().top);
+//alert($(".item-skills").offset().top);
+	var sc_height_item_skills = $(" .item-skills ").offset().top; 
+	if ($(" .item-skills ").next().hasClass('progressing-bar-in')) {
+		alert("yes");
+	}
+	else{
+		alert('no');
+	} 
+
+		//alert(sc_height_item_skills);
+	if ($(window).scrollTop() === sc_height_item_skills ) {
+		alert('good');
 		$(".progressing-bar-in").css({"width": "70%"});
 	
 	}

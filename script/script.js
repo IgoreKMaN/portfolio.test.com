05849b56cc5bd198b47e126d56f08aec77f33875
y1876 <y1876@mail.ru>
2018-11-25 22:19:24 +0300

    section portfolio

--- script/script.js
+++ script/script.js
@@ -2,17 +2,33 @@ $(document).ready(function(){
 	var height_block = $(".container-header-next").height();
 	$("section .o-section-header, section .o-section-content").height(height_block);
 
-	var scrollCss = $(".item-skills").offset().top - $(".skills").height() - $(".css").height()*2;
-	console.log(scrollCss);
+	var scrollCss = $(".css").offset().top - $(".css").width(); 
+		
 	$(document).scroll(function(){
-		if ($(document).scrollTop() >= scrollCss ) {
-			//alert("Это работает");
-			$(".css .progressing-bar-in").css({"width": "90%"});
-			$(".js .progressing-bar-in").css({"width": "60%"});		
-		}
-	})
+		var scrollTopWindow = $(document).scrollTop() + $(window).height(),
+			cssOT = $(".css").offset().top + $(".css").height(),
+			gitOT = $(".git").offset().top + $(".git").height(),
+			photoshopOT = $(".photoshop").offset().top + $(".photoshop").height(),
+			cssText = $(".css>p>span").text(),
+			gitText = $(".git>p>span").text(),
+			photoshopText = $(".photoshop>p>span").text(),
+			jsText = $(".js>p>span").text(),
+			videoText = $(".video>p>span").text(),
+			animationText = $(".animation>p>span").text();
+		
 
-	/*$(".o-section-header").height(100%);*/
-	
+		if(scrollTopWindow > cssOT){
+			$(".css .progressing-bar-in").css({"width": cssText});
+			$(".js .progressing-bar-in").css({"width": jsText});	
+		}
+		if(scrollTopWindow > gitOT){
+			$(".git .progressing-bar-in").css({"width": gitText});
+			$(".video .progressing-bar-in").css({"width": videoText});	
+		}
+		if(scrollTopWindow > photoshopOT){
+			$(".photoshop .progressing-bar-in").css({"width": photoshopText});
+			$(".animation .progressing-bar-in").css({"width": animationText});	
+		}
+	})	
 
 })
